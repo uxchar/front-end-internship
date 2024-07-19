a3c218975f9025580f56f175250c64ddb6feeca4
uxchar <chauncey.harlan.1@gmail.com>
2024-07-19 10:39:07 -0400

    fixed activity 6

--- Assignments/JavaScript/Activity 7 - FizzBuzz/scripts.js
+++ Assignments/JavaScript/Activity 7 - FizzBuzz/scripts.js
@@ -9,12 +9,12 @@
 for (x = 1; x <= 100; x++) {
   if (x % 3 == 0 && x % 5 == 0) {
     console.log(`Fizz Buzz`);
-  } else if (x % 3 == 0 && x % 5 !== 0) {
+  } else if (x % 3 == 0) {
     console.log(`Fizz`);
-  } else if (x % 3 !== 0 && x % 5 == 0) {
+  } else if (x % 5 == 0) {
     console.log(`Buzz`);
   } else {
-    console.log(`${x}`);
+    console.log(x);
   }
 }
 
